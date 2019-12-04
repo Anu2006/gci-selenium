91e4f54f7b997365265dfd7cdf00c6b28e99f794
Anu2006 <55542548+Anu2006@users.noreply.github.com>
2019-12-04 15:48:08 +0530

    Add files via upload

--- codeforces.py
+++ codeforces.py
@@ -1,13 +1,31 @@
+from multiprocessing.pool import ThreadPool
 from selenium.webdriver import Chrome
+import getpass
+
+def get_elements():
+    yield from (driver.find_element_by_name(element_name)
+                for element_name in element_names)
+
+pool = ThreadPool(processes=1)
+async_result = pool.apply_async(get_elements)
+element_names = 'handle email password passwordConfirmation'.split()
+# input_values = ['Anu2006', 'ranilcd@gmail.com', 'ranil1974', 'ranil1974']
+input_values = []
+for info in ['user name'] + element_names[1:]:
+    if 'password' in info:
+        input_values.append(getpass.getpass(f'{info}: '))
+
+    else:
+        input_values.append(input(f'{info}: '))
+
+if not input_values[2] == input_values[3]:
+    raise ValueError('Enter similar passwords to last two fields')
+# do some other stuff in the main process
+
+elements = async_result.get()
 
 driver = Chrome(executable_path='C:/Users/Ewis/Downloads/chromedriver.exe')
 driver.get('https://codeforces.com/register')
-
-element_names = ['handle', 'email', 'password', 'passwordConfirmation']
-input_values = ['Anu2006', 'ranilcd@gmail.com', 'ranil1974', 'ranil1974']
-
-elements = (driver.find_element_by_name(element_name)
-            for element_name in element_names)
 
 for element, val in zip(elements, input_values):
     element.send_keys(val)
